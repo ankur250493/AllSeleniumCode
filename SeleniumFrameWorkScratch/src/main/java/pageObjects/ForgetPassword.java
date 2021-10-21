c50bb1441df929d23056f65adf73a3b24f676b31
ankur250493 <ank11.thakur@gmail.com>
2021-10-22 02:30:49 +0530

    initial commit

---
+++ SeleniumFrameWorkScratch/src/main/java/pageObjects/ForgetPassword.java
@@ -0,0 +1,25 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ForgetPassword {
+
+    public WebDriver driver;
+    By email = By.name("email");
+    By sendMeInstructions = By.name("commit");
+    public ForgetPassword(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement enterEmail() {
+        return driver.findElement(email);
+
+    }
+
+    public WebElement submitEmail() {
+        return driver.findElement(sendMeInstructions);
+
+    }
+}
